c426cae0989171c9adcb8b06cf4a7c2032035f5d
pragatibaderiya27 <93194017+pragatibaderiya27@users.noreply.github.com>
2025-05-16 16:16:59 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 {
  
  
-     int arr[] = {10, 20, 30, 40, 50};
+     int arr[] = {10, 20, 30, 40, 50 60};
      int n = sizeof(arr) / sizeof(arr[0]);  // Calculate the number of elements
  
      // Traverse the array using a for loop
@@ -24,11 +24,14 @@ int main(int argc, char const *argv[])
  cout<<"whats your score at third position"<<endl;
  cout<<"whats your score at fourt position"<<endl;
  cout<<"whats your score at fifth position"<<endl;
+ cout<<"whats your score at sixst position"<<endl;
+
  cin>> my_first_array[0];
  cin>> my_first_array[1];
  cin>> my_first_array[2];
  cin>> my_first_array[3];
  cin>> my_first_array[4]; 
+cin>> my_first_array[5]
  /*vector<char> vowels ['a','e','i','o','u'];
  vector<int> test_score [80, 120, 150, 85, 50];
  vector<double> temprature (100,85.20);*/
